66b21468d2746d29f843ed4b8e7e639092528532
Musa C. Yurt <yurtmusa@outlook.com>
2024-03-05 21:04:19 +0000

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Musa Canberk Yurt",
+  title: "Developer & Designer & Drone Operator",
+  email: "yurtmusa@outlook.com",
+  gitHub: "uaaeu",
+  instagram: "musacyurt",
+  linkedIn: "musayurt",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "yurtmusa",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
